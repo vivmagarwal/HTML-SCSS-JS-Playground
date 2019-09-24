17ed120bd4e7936d6917c7f0726396dea8597999
drupalastic <vivsurat@gmail.com>
2019-09-24 10:09:29 +0530

    kata-2: binary search.

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ index.html and script.js is used as it is.
 JS files are not processed because most browsers supports latest JS features. In case you are an IE-11 user - you may consider using https://github.com/drupalastic/HTML-SCSS-JS-Starterkit 
 
 style.scss will be compiled to style.css. Browser will refresh automatically on change. 
+
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-console.log('hello world18');
-
-for (let i=0; i<5; i++){
-  console.log(i);
+// liner search
+function linerSearch(list, item) {
+  let index = -1;
+  list.forEach((listItem,i) => {
+    if (listItem === item){
+      index = i;
+    }
+  });
+  return index;
 }
+
+console.log('liner search => ', linerSearch([2, 6, 7, 90, 103], 90));
+
+// Binary search
+function binarySearch(list, item) {
+
+}
+
+console.log(binarySearch([2,6,7,90,103,105],90));
